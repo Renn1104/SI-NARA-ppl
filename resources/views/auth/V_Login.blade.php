8eb4fc5179d104b204b66f681bafcda3c1edb31f
Rendy <rendy.pramudya30@gmail.com>
2025-04-22 10:31:01 +0700

    update

--- resources/views/auth/V_Login.blade.php
+++ resources/views/auth/V_Login.blade.php
@@ -31,7 +31,7 @@
           <label class="block font-semibold text-gray-700 mb-1">Password</label>
           <div class="relative">
           <input type="text" name="password" placeholder="Masukkan Password" required class="w-full border border-gray-300 rounded px-4 py-2 pr-10 focus:outline-none focus:ring-2 focus:ring-purple-600">
-            <!-- <span onclick="togglePassword()" class="absolute right-3 top-2.5 text-gray-500 cursor-pointer text-xl select-none">👁️</span> -->
+            <span onclick="togglePassword()" class="absolute right-3 top-2.5 text-gray-500 cursor-pointer text-xl select-none">👁️</span>
           </div>
       </div>
 
